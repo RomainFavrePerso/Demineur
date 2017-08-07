f24311cd733a6d17f503c2aef956d4c1370432ee
Romain <romain.favre@epitech.eu>
2017-08-07 21:34:18 +0200

    Modèle des cases du démineur

---
+++ Demineur/Demineur/Models/Case.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demineur.Models
+{
+    class Case
+    {
+        public int posX;
+        public int posY;
+
+        public int? caseValue;
+
+        public bool isMine
+        {
+            get
+            {
+                return caseValue == null;
+            }
+        }
+
+        Case(int x, int y, int? casevalue = null)
+        {
+            posX = x;
+            posY = y;
+            caseValue = casevalue;
+        }
+    }
+}
